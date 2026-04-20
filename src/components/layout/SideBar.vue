023905ac34050deda17d41b7978527330741e0a2
nazeerou <naziru2007@gmail.com>
2026-04-20 10:56:19 +0300

    build for prod

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -102,15 +102,15 @@
             <i class="fas fa-plus-circle"></i>
             <span>Ombi la Mkopo</span>
           </router-link>
-          <!-- <router-link to="/loans/pending" class="submenu-item" @click="closeSidebarOnMobile">
+          <router-link to="/loans/pending" class="submenu-item" @click="closeSidebarOnMobile">
             <i class="fas fa-clock"></i>
             <span>Mikopo Yetu</span>
             <span v-if="pendingLoansCount" class="badge">{{ pendingLoansCount }}</span>
-          </router-link> -->
+          </router-link>
           <router-link to="/loans/penalty" class="submenu-item" @click="closeSidebarOnMobile">
             <i class="fas fa-clock"></i>
             <span>Mikopo Iliyopitiliza (Penalty)</span>
-            <!-- <span v-if="pendingLoansCount" class="badge">{{ pendingLoansPenalty }}</span> -->
+            <span v-if="pendingLoansCount" class="badge">{{ pendingLoansPenalty }}</span>
           </router-link>
         </div>
       </div>
